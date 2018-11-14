6b85042e497e937959992f483c44e0ed83668a84
unknown <iziken@inbox.ru>
2018-11-14 22:49:55 +0700

    first commit

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function MessageHistory( {list} ) {
+  if (list.length === 0 || !(list instanceof Array)) {
+    return false;
+  }
+
+  let history = list.map( (message, index) => {
+    switch (message.type) {
+      case 'message':
+        return <Message from = {message.from} message = {message} key = {message.id} />
+        break;
+      case 'response':
+        return <Response from = {message.from} message = {message} key = {message.id} />
+        break;
+      case 'typing':
+        return <Typing from = {message.from} message = {message} key = {message.id} />
+        break;
+    }
+
+  });
+  return <ul>{history}</ul>;
+}
