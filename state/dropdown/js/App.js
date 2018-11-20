c0183c156ebed88e4fadaec9c5a507890c9547e8
unknown <iziken@inbox.ru>
2018-11-20 14:35:01 +0700

    change

--- state/dropdown/js/App.js
+++ state/dropdown/js/App.js
@@ -1,24 +1,28 @@
-var App = React.createClass({
-  defaultProps: {
-    options: []
-  },
-  getInitialState: function () {
-    return {
+'use strict'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       active: this.props.options[0],
       open: false
-    };
-  },
-  handleChange: function (option) {
+    }
+  }
+
+
+  handleChange = option => {
     this.setState({
       active: option
     });
-  },
-  toggleOpen: function () {
+  };
+
+  toggleOpen = () => {
     this.setState({
       open: !this.state.open
     });
-  },
-  render: function () {
+  };
+
+  render() {
     return (
       <div className="container">
         <div className={`dropdown-wrapper ${this.state.open ? "open" : ""}`} >
@@ -39,4 +43,4 @@ var App = React.createClass({
       </div>
     );
   }
-});
+}
