06b1b6d6873a13d5fd0e3a22547afcf4fbedd022
unknown <iziken@inbox.ru>
2018-12-03 14:35:30 +0700

    change

--- routing/tabs/js/App.js
+++ routing/tabs/js/App.js
@@ -1,1 +1,22 @@
+'use strict';
 
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="tabs">
+          <nav className="tabs__items">
+            <NavLink to="/" exact className="tabs__item" activeClassName="tabs__item-active">Рефераты</NavLink>
+            <NavLink to="/creator" className="tabs__item" activeClassName="tabs__item-active">Криэйтор</NavLink>
+            <NavLink to="/fortune" className="tabs__item" activeClassName="tabs__item-active">Гадалка</NavLink>
+          </nav>
+        <div class="tabs__content">
+          <Route path="/" exact component={Essay} />
+          <Route path="/creator" component={Creator} />
+          <Route path="/fortune" component={Fortune} />
+        </div>
+        </div>
+      </Router>
+    );
+  }
+}
